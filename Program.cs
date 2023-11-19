0d93feb32b7a90cf9a6b3ebffb7b99a3d22170fe
Esho <esho.goria2@gmail.com>
2023-11-19 18:54:05 +0100

    Added using system and changed using System.IO.Enumeration to using System.IO

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO.Enumeration;
+using System.IO;
 
 namespace MJU23v_DTP_T2
 {
